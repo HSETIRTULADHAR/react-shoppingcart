a5cc8a77d6d3cce0ee33d9e61fccdbc51c525d21
Ritesh <riteshtuladhar19961205@gmail.com>
2021-05-08 16:27:11 +0545

    create product component

---
+++ src/components/Products.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export class Products extends Component {
+    render() {
+        return (
+            <div>
+                <ul className="products">
+                   {this.props.products.map( product => (
+                       <li key={product._id} className="product">
+                           
+                               <a href={"#" + product._id}>
+                                   <img src={product.image} alt={product.title}></img>
+                                      {product.description}
+                               </a>
+                               <div className="product-price">
+                                  ${product.price}
+                                   <button>
+                                       Add to Cart
+                                   </button>
+                               </div>
+                       </li>
+                   )
+
+                   )}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Products
